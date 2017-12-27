035c6fc814cc7f38021acaaeb786f4d69abb4284
Frederic MAMATH <fredericmamath@gmail.com>
2017-12-27 22:04:31 +0100

    :tada: Finished boilerplate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+/* global document */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import configureStore, { history } from './store';
+
 import App from './App';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
+
+registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import { routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+
+import rootReducer from './reducers';
+
+export const history = createHistory();
+
+const configureStore = () => {
+  const middlewares = [];
+
+  middlewares.push(routerMiddleware(history));
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(createLogger());
+  }
+
+  return createStore(
+    rootReducer,
+    applyMiddleware(...middlewares),
+  );
+};
+
+export default configureStore;
