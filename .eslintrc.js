617605da3f730d832b739292173610c626381190
Frederic MAMATH <fredericmamath@gmail.com>
2017-12-27 21:46:30 +0100

    :wrench: Added linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    rules: {
+        "linebreak-style": 0,
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "jsx-a11y/anchor-is-valid": [ "error", {
+            "components": [ "Link" ],
+            "specialLink": [ "to" ],
+        }],
+    }
 };
